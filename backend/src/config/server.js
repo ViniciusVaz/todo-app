1e4b8d46a9f5c73b0aef26cee620534beebde18b
Vinicius Vaz <viniciushenriquevaz@hotmail.com>
2018-04-15 17:04:42 -0300

    - Created middleware Cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,9 +2,11 @@ const port = 4004
     , bodyParser = require('body-parser')
     , express = require('express')
     , server = express()
+    , allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, () => {
     console.log(`Backend on port: ${port}`)
